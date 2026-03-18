ca578a5fb777b309abed36934fc3610326915e68
Greg W <1514406+fatbotgw@users.noreply.github.com>
2026-03-17 21:04:15 -0400

    added handler for readiness endpoint

--- main.go
+++ main.go
@@ -12,13 +12,20 @@ func main () {
 
 	httpServerMux := http.NewServeMux()
 	httpServerMux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot))))
+	httpServerMux.HandleFunc("/healthz", health)
+
 
 	httpServer := http.Server {
 		Addr: ":" + port,
 		Handler: httpServerMux,
 	}
-	
 
 	log.Printf("\nServing files from %s on port: %s\n", filepathRoot, port)
 	log.Fatal(httpServer.ListenAndServe())
+}
+
+func health(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)  // 200 OK status code
+	w.Write([]byte("OK"))
 }
