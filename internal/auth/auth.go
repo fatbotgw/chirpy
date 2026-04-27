59d6588b78230efa1c7e5f2eaa12aa3e0aeece15
Greg W <1514406+fatbotgw@users.noreply.github.com>
2026-04-26 20:46:20 -0400

    Added password hash function

---
+++ internal/auth/auth.go
@@ -0,0 +1,27 @@
+package auth
+
+import (
+	"github.com/alexedwards/argon2id"
+)
+
+func HashPassword(password string) (string, error) {
+	//  Hash the password using the argon2id.CreateHash function
+	hash, err := argon2id.CreateHash(password, argon2id.DefaultParams)
+	if err != nil {
+		return "", err
+	}
+	return hash, nil
+}
+
+func CheckPasswordHash(password, hash string) (bool, error) {
+	// Use the argon2id.ComparePasswordAndHash function to compare the 
+	// password that the user entered in the HTTP request with the password 
+	// that is stored in the database
+	_, err := argon2id.ComparePasswordAndHash(password, hash)
+	if err != nil {
+		return false, err
+	}
+
+	return true, nil
+}
+
